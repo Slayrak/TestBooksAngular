5c79d5e7cd78ad20df82113c5543139296678234
Максим Шимків <322topgamerintheworld228@gmail.com>
2023-02-27 23:55:13 +0200

    First step

--- TestBooksAngular/TestBooksAngular/src/app/app.module.ts
+++ TestBooksAngular/TestBooksAngular/src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BooksPageComponent } from './components/books-page/books-page.component';
+import { BooksListComponent } from './components/books-list/books-list.component';
+import { BooksListItemComponent } from './components/books-list-item/books-list-item.component';
+import { EditBookComponent } from './components/edit-book/edit-book.component';
+import { ViewBookComponent } from './components/view-book/view-book.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BooksPageComponent,
+    BooksListComponent,
+    BooksListItemComponent,
+    EditBookComponent,
+    ViewBookComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ TestBooksAngular/TestBooksAngular/src/app/components/books-list-item/books-list-item.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-books-list-item',
+  templateUrl: './books-list-item.component.html',
+  styleUrls: ['./books-list-item.component.scss']
+})
+export class BooksListItemComponent {
+
+}
